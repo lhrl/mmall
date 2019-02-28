6096cd4617c20be0d04b959bec87ce293597dead
lhrl <18221482595@163.com>
2019-02-28 22:59:45 +0800

    “打印日志”

--- src/main/java/com/lhrl/controller/UserController.java
+++ src/main/java/com/lhrl/controller/UserController.java
@@ -4,4 +4,8 @@ package com.lhrl.controller;
  * Created by liulang on 2019/2/28.
  */
 public class UserController {
+
+    public static void main(String[] args) {
+        System.out.println("hell world");
+    }
 }
